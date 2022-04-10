76308a5957a796035a7654c95bff47eccaf4cdc3
michael shohat <michaelsho9@gmail.com>
2022-04-10 12:44:42 +0300

    added requirements' installation scripts

---
+++ install_requirements.bat
@@ -0,0 +1,6 @@
+pip install kivy
+pip install pyttsx3
+pip install SpeechRecognition
+pip install pipwin
+pipwin install pyaudio
+pipwin install pocketsphinx
---
+++ install_requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install kivy
+pip install pyttsx3
+pip install SpeechRecognition
+pip install pipwin
+pip install pyaudio
+pip install pocketsphinx
